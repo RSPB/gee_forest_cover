ff8a24933b01a352aca7b42a056d304332d300fe
tracek <lukasz.tracewski@outlook.com>
2019-12-17 23:26:58 +0100

    loss proc

--- calc_loss.js
+++ calc_loss.js
@@ -1,29 +1,43 @@
 var gfc = ee.Image("UMD/hansen/global_forest_change_2018_v1_6"),
     large = ee.ImageCollection("users/rspb/birds_large_2019");
 
-var scale = 30;
+var scale = 480;
 var maxPixels = 1e12;
 
 var forest2000 = gfc.select(['treecover2000']).divide(100);
+var lossYear = gfc.select('lossyear');
 var areaImage2000 = forest2000.multiply(ee.Image.pixelArea()); 
 
-var features = large.map(function f(image)
-{
-  var aoo = areaImage2000.mask(image);
+function compute_forest_area(forest, year) {
+  var forest_area_co = large.map(function f(image)
+  {
+    var loss_area = forest.mask(image).reduceRegion({
+      geometry: image.geometry(scale),
+      reducer: 'sum',
+      scale: scale,
+      maxPixels: maxPixels
+    }).get('treecover2000');
+    
+    var key = 'loss_' + year.toString();
+    var d = {}
+    d[key] = loss_area
+    
+    return ee.Feature(null, ee.Dictionary(d));
   
-  var treecover2000 = aoo.reduceRegion({
-    geometry: image.geometry(scale),
-    reducer: 'sum',
-    scale: scale,
-    maxPixels: maxPixels
-  }).get('treecover2000');
+  });
   
-  return ee.Feature(null, ee.Dictionary({'name': image.get("system:index"), 'treecover2000': treecover2000}));
-});
+  return forest_area_co;
+}
 
-Export.table.toDrive({
-  collection: features,
-  description: 'large_' + scale.toString(),
-  fileFormat: 'CSV',
-  folder: 'EarthEngine'
-});
+for (var year = 1; year <= 18; year++) {
+  var year_full = 2000 + year;
+  var areaLossImage2000 = forest2000.mask(lossYear.eq(year)).multiply(ee.Image.pixelArea());
+  var co = compute_forest_area(areaLossImage2000, year);
+  
+  Export.table.toDrive({
+    collection: co,
+    description: 'large_loss_' + year_full + '_' + scale.toString(),
+    fileFormat: 'CSV',
+    folder: 'EarthEngine'
+  });
+}
